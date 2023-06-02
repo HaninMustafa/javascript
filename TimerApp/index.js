0133818b0e5f50d73f3b21e106f0d5d84e0fc6f1
Hanin Mustafa <haninmustafa13@gmail.com>
2023-06-02 05:25:03 +0300

    adding onTick onStart and onComplete

--- TimerApp/index.js
+++ TimerApp/index.js
@@ -1,18 +1,27 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  //the callbacks are optional
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
+    //we need to check if onStart is added as it is optional
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     //will run the function once every second .. but it will wait one sec!
     // const interval = setInterval(this.tick, 1000);
-
     //to make the variable shared to the pause function we change it to this.interval
     this.interval = setInterval(this.tick, 1000);
   };
@@ -24,8 +33,14 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
   //Getters
@@ -42,4 +57,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer onTick");
+  },
+  onComplete() {
+    console.log("Timer onComplete");
+  },
+});
